f66b2784fce06017d4835699d0464b5dca0add84
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2023-04-21 08:04:52 +0200

    implement basic mem_mgr

--- src/mem_mgr.h
+++ src/mem_mgr.h
@@ -17,11 +17,12 @@ typedef struct {
 /**
  * @brief initializes shared memory
  *
- * @param stats when NULL link to memory, otherwise initialize new memory based
- *              on the parameters in stats
+ * @param stats initialize new memory based on the parameters in stats
+ * @param init true if this is the first inicialization of the memory
+ *             only one process should call this with init set to true
  * @return _Bool true on success, otherwise false
  */
-_Bool mmgr_init(mmgr_stats *stats);
+_Bool mmgr_init(mmgr_stats *stats, _Bool init);
 
 // get the stats (only for reading)
 const mmgr_stats *mmgr_g_stats();
@@ -36,6 +37,7 @@ void mmgr_r_log_file();
  * @brief closes the shared memory
  *
  * @param clear if this is true, the memory will also be freed
+ *              only one process should call this with true
  */
 void mmgr_close(_Bool clear);
 
--- src/mem_mngr.c
+++ src/mem_mngr.c
@@ -1,22 +1,98 @@
 #include "mem_mgr.h" // mgr_stats, size_t
 
-#include <semaphore.h> // sem_t
-#include <sys/mman.h>  // shm_open
-#include <fcntl.h>     // O_RDWR, O_CREAT
+#include <semaphore.h> // sem_t, sem_init
+#include <sys/mman.h>  // shm_open, PROT_READ, PROT_WRITE, MAP_SHARED,
+                       // MAP_FAILED, shm_unlink, mmap
+#include <fcntl.h>     // O_RDWR, O_CREAT, O_EXCL
 #include <sys/stat.h>  // S_IRUSR, S_IWUSR
+#include <unistd.h>    // ftruncate, close
 
 #define SH_MEM_NAME "/ios-p2"
 
 // all the things in the shared memory
 typedef struct {
+    size_t mem_size;
     mmgr_stats stats;
     sem_t log_file_cnt_sem;
     size_t log_file_cnt;
-} shared_memory;
+} mmgr_memory;
 
-static shared_memory *mem = NULL;
+static mmgr_memory *mem = NULL;
 
-_Bool mmgr_init(mmgr_stats *stats) {
-    int fd = shm_open(SH_MEM_NAME, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    // TODO (change - stats cannot be NULL)
+_Bool _mmgr_init(mmgr_stats *stats, size_t mem_size);
+
+_Bool mmgr_init(mmgr_stats *stats, _Bool init) {
+    // calculate the required space
+    size_t size = sizeof(mmgr_memory);
+
+    int oflags = O_RDWR;
+    if (init)
+        oflags |= O_EXCL | O_CREAT;
+
+    // create shared memory
+    int fd = shm_open(SH_MEM_NAME, oflags, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        return 0;
+
+    // resize the memory if it has not been initialized
+    if (init && ftruncate(fd, size) == -1) {
+        close(fd);
+        shm_unlink(SH_MEM_NAME);
+        return 0;
+    }
+
+    // link the memory
+    mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    close(fd);
+    if (mem == MAP_FAILED) {
+        if (init)
+            shm_unlink(SH_MEM_NAME);
+        return 0;
+    }
+
+    // initialize the memory contents on the firs init
+    if (init && !_mmgr_init(stats, size)) {
+        munmap(mem, size);
+        shm_unlink(SH_MEM_NAME);
+        return 0;
+    }
+
+    return 1;
+}
+
+// initializes the memory contents
+_Bool _mmgr_init(mmgr_stats *stats, size_t mem_size) {
+    // init the semaphores
+    if (sem_init(&mem->log_file_cnt_sem, 1, 1) == -1)
+        return 0;
+
+    // set the default values
+    mem->mem_size = mem_size;
+    mem->stats = *stats;
+    mem->log_file_cnt = 0;
+}
+
+void mmgr_close(_Bool clear) {
+    if (!clear) {
+        munmap(mem, mem->mem_size);
+        return;
+    }
+
+    sem_close(&mem->log_file_cnt_sem);
+
+    munmap(mem, mem->mem_size);
+    shm_unlink(SH_MEM_NAME);
+}
+
+const mmgr_stats *mmgr_g_stats() {
+    return &mem->stats;
+}
+
+size_t *mmgr_g_log_file() {
+    sem_wait(&mem->log_file_cnt_sem);
+    return &mem->log_file_cnt;
+}
+
+void mmgr_r_log_file() {
+    sem_post(&mem->log_file_cnt_sem);
 }
