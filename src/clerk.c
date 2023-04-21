3db726fafe6e124ac1ecaef4e65f1f447e3535a6
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2023-04-21 19:47:07 +0200

    Create unique random seed for each process

--- src/clerk.c
+++ src/clerk.c
@@ -2,11 +2,17 @@
 
 #include <stdlib.h> // EXIT_SUCCESS
 #include <unistd.h> // usleep
+#include <stdlib.h> // srand, rand
+#include <time.h>   // time
 
-#include "logger.h" // flog
+#include "logger.h"  // flog
+#include "mem_mgr.h" // mmgr_g_stats
 
 int clerk_main(size_t id) {
-    usleep(id * 50);
-    flog("Clerk id: %zu", id);
+    // make unique seed
+    srand(time(NULL) * id + 11);
+
+    usleep(rand() % 100000);
+    flog("Clerk id: %zu/%zu", id, mmgr_g_stats()->nu);
     return EXIT_SUCCESS;
 }
--- src/customer.c
+++ src/customer.c
@@ -2,11 +2,17 @@
 
 #include <stdlib.h> // EXIT_SUCCESS
 #include <unistd.h> // usleep
+#include <stdlib.h> // srand, rand
+#include <time.h>   // time
 
-#include "logger.h" // flog
+#include "logger.h"  // flog
+#include "mem_mgr.h" // mmgr_g_stats
 
 int customer_main(size_t id) {
-    usleep(id * 80);
-    flog("Customer id: %zu", id);
+    // make unique seed
+    srand(time(NULL) * id + 17);
+
+    usleep(rand() % 100000);
+    flog("Customer id: %zu/%zu", id, mmgr_g_stats()->nz);
     return EXIT_SUCCESS;
 }
